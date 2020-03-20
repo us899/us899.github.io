7b4856af91f83871135babd9ff9d269b7177ad73
Utsav Singh <48082371+us899@users.noreply.github.com>
2020-03-20 20:59:25 +0530

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -8,8 +8,8 @@
     $email_subject = "Contact Message from ".$visiter_email;
 
     $headers = "From: ".$email_from;
-    $headers .= "Reply-to: ".visiter_email;
-    $txt = "Name: ".$name."\nEmail: ".$visiter_email."\nMessage:\n".message;
+    //$headers .= "Reply-to: ".visiter_email;
+    $txt = "Name: ".$name."\nEmail: ".$visiter_email."\nMessage:\n".$message;
 
     mail($email_to, $email_subject, $txt, $headers);
     header("Location: index.html");
